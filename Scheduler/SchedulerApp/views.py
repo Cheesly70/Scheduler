4fd37e32d806ec1ffdae917f3cc8e1c7a0c8b338
Chelsey <chioma.ezete@uconn.edu>
2017-01-07 23:57:09 -0500

    updated the view to perform httpResponse test

--- Scheduler/SchedulerApp/views.py
+++ Scheduler/SchedulerApp/views.py
@@ -7,3 +7,6 @@ def index(request):
     #return HttpResponse("<h2>HEY!</h2>")
     return render(request, 'SchedulerApp/home.html') #{'form': form,
                                                       #'class_num': classnum,})
+
+def courses(request):
+    return HttpResponse("testing testing 123")
