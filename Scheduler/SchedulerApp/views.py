12d740ef84144bc2af27e90b2c6336f3b273387d
Chelsey <chioma.ezete@uconn.edu>
2017-01-10 00:00:08 -0500

    added psuedocode logic for topological sort

--- Scheduler/SchedulerApp/views.py
+++ Scheduler/SchedulerApp/views.py
@@ -24,4 +24,27 @@ def getmainform(request):
 
 def processmainform(request):
     # here is where the top sort logic goes
-    return HttpResponse("<h1>testing testing 123</h1>")
+    #return HttpResponse("<h1>testing testing 123</h1>")
+
+    '''
+    General Idea:
+        -Create a graph based on input
+        -Use DFS to go deep into the graph. When you can't go any further
+        you've found the sink node (that you mark with the predefined local var)
+    Pseudocode for top sort (this method takes a graph):
+        - Mark all nodes unvisited
+        - create local var that will represent ordering_label that will be applied to each node
+        - Since you can start anywhere in the graph we can do the following:
+            - for each vertex v in graph G
+                - if v is not yet explored
+                    - DFS on (G,v)
+    Pseudocode for DFS (this method takes a graph and a vertex v):
+        - mark v explored
+        - for each edge (v,u)
+            - if u is unexplored
+                - DFS(G,u)
+        *****we get here because there are no outgoing arcs*****
+        - set the label for the current vertex (local var we defined in the top sort method)
+        - decrement the label so that the next deepest node gets that ordering value
+
+    '''
